5eaa7b22e7bae953b2b7c027906b17a6dcfbbdef
prashantmane091318 <61352956+prashantmane091318@users.noreply.github.com>
2020-07-07 06:43:21 -0700

    Update salary.py

--- salary.py
+++ salary.py
@@ -17,14 +17,7 @@ def fun2():
     val=round(model.predict([[exp]])[0])
     return "salary of {} is:{}".format(name,val)
 
-@app.route("/api",methods=["post","get"])
-def fun3():
-    data=request.get_json()
-    name=data["name"]
-    exp=float(data["exp"])
-    model=pickle.load(open("mysalary.pkl","rb"))
-    val=round(model.predict([[exp]])[0])
-    return jsonify({"name":name,"salary":val})
+
 
 if __name__=="__main__":
     app.run(debug=True)
